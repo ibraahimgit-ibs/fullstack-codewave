b26530388fc1aeacd8c79795c04835d51c519af2
ibraahimgit-ibs <ibraahimgit@gmail.com>
2026-01-22 10:48:19 +0300

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,76 +1,15 @@
 import express from 'express'
-import pool from './config/db.js'
 import { port as PORT } from './config/config.js'
-import chalk from 'chalk'
 import cors from 'cors'
-import bcrypt from 'bcrypt'
 import axios from 'axios'
+import pool from './config/db.js';
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174']
+  origin: ['http://localhost:5173', 'http://localhost:5174', "https://myapp.vercel.app"]
 }
 
 const app = express()
 app.use(express.json())
 app.use(cors(corsOptions))
-const saltRounds = 10
-
-// GET all users
-// app.get('/users', async (req, res) => {
-//   try {
-//     const result = await pool.query('SELECT * FROM users')
-//     console.log(chalk.blue('DB result rows:'), result.rows)
-
-//     if (result.rows.length === 0) {
-//       console.log(chalk.red('No users found in the database.'))
-//     } else {
-//       console.log(
-//         chalk.green(`Fetched ${result.rows.length} users from the database.`)
-//       )
-//     }
-
-//     res.json(result.rows)
-//   } catch (err) {
-//     console.error(err)
-//     res.status(500).json({ error: 'DB error' })
-//   }
-// })
-
-// // POST new user
-// app.post('/users', async (req, res) => {
-//   const { name, email, password } = req.body;
-//   try {
-//     const hashedPassword = await bcrypt.hash(password, saltRounds)
-
-//     const result = await pool.query(
-//       'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
-//       [name, email, hashedPassword]
-//     )
-//     res.json(result.rows[0])
-//   } catch (err) {
-//     console.error(err)
-//     res.status(500).json({ error: 'DB insert error' })
-//   }
-// })
-
-// // DELETE user by ID
-// app.delete('/users/:id', async (req, res) => {
-//   const { id } = req.params
-//   try {
-//     const result = await pool.query(
-//       'DELETE FROM users WHERE id = $1 RETURNING *',
-//       [id]
-//     )
-
-//     if (result.rows.length === 0) {
-//       return res.status(404).json({ error: 'User not found' })
-//     }
-
-//     res.json({ message: 'User deleted successfully', user: result.rows[0] })
-//   } catch (err) {
-//     console.error(err)
-//     res.status(500).json({ error: 'DB delete error' })
-//   }
-// })
 
 app.get('/ask', async (req, res) => {
   const question = req.query.question
